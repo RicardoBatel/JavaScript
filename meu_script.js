7f9391b64b77eccbb762d7795dd8ca0ddd6529a8
RicardoBatel <112258124+RicardoBatel@users.noreply.github.com>
2022-09-26 15:32:02 -0300

    primeiro commit

---
+++ meu_script.js
@@ -0,0 +1,8 @@
+//alert('Olá, seja bem vindo ao curso!!!')
+
+/*
+    selecionar em elemento no DOM
+    atualizar o valor desse elemento com uma string
+*/
+
+document.getElementById('nome').value = 'oi'
